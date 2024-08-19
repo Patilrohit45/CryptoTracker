eb6ab6238d241834c16f365eacf9fab26869fa2f
Patilrohit45 <patilrohit.official@gmail.com>
2024-08-19 14:17:48 +0530

    fetched details from api and map it in model and show it on UI.

---
+++ CryptoTracker/CryptoTracker/Core/Detail/ViewModels/DetailViewModel.swift
@@ -0,0 +1,102 @@
+//
+//  DetailViewModel.swift
+//  CryptoTracker
+//
+//  Created by Rohit Patil on 19/08/24.
+//
+
+import Foundation
+import Combine
+
+
+class DetailViewModel : ObservableObject{
+    
+    @Published var overviewStatistics: [StatisticsModel] = []
+    @Published var additionalStatistics: [StatisticsModel] = []
+    
+    @Published var  coin:CoinModel
+    private let coinDetailService:CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+    
+    
+    init(coin:CoinModel){
+        self.coin = coin
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers(){
+        coinDetailService.$coinDetails
+            .combineLatest($coin)
+            .map(mapDataToStatistics)
+            .sink { [weak self] (returnedArrays) in
+                self?.overviewStatistics = returnedArrays.overview
+                self?.additionalStatistics = returnedArrays.addional
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func mapDataToStatistics(coinDetailModel:CoinDetailModel?, coinModel:CoinModel) -> (overview:[StatisticsModel],addional:[StatisticsModel]) {
+        
+        let overviewArray = createOverviewArray(coinModel: coinModel)
+        
+        let addionalArray = createAdditionalArray(coinDetailModel: coinDetailModel, coinModel: coinModel)
+        
+        return (overviewArray,addionalArray)
+    }
+    
+    private func createOverviewArray(coinModel:CoinModel) -> [StatisticsModel] {
+        let price = coinModel.currentPrice.asCurrencyWith6Decimals()
+        let pricePercentageChange = coinModel.priceChangePercentage24H
+        let priceStat = StatisticsModel(title: "Current Price", value: price,percentageChange: pricePercentageChange)
+        
+        let marketCap = "$" + (coinModel.marketCap?.formattedWithAbberviations() ?? "")
+        let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+        let marketCapStat = StatisticsModel(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentChange)
+        
+        let rank = "\(coinModel.rank)"
+        let rankStat = StatisticsModel(title: "Rank", value: rank)
+        
+        let volume = "$" + (coinModel.totalVolume?.formattedWithAbberviations() ?? "")
+        let volumeStat = StatisticsModel(title: "Volume", value: volume)
+        
+        let overviewArray  : [StatisticsModel] = [
+            priceStat,marketCapStat,rankStat,volumeStat
+        ]
+        
+        return overviewArray
+        
+    }
+    
+    private func createAdditionalArray(coinDetailModel:CoinDetailModel?,coinModel:CoinModel) -> [StatisticsModel] {
+    
+        let high = coinModel.high24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let hightStat = StatisticsModel(title: "24h High", value: high)
+        
+        let low = coinModel.low24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let lowStat = StatisticsModel(title: "24h Low", value: low)
+        
+        
+        let priceChange = coinModel.priceChange24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let pricePercentageChange = coinModel.priceChangePercentage24H
+        let priceChangeStat = StatisticsModel(title: "24h Price Change", value: priceChange, percentageChange: pricePercentageChange)
+        
+        let marketCapChange = "$" + (coinModel.marketCapChangePercentage24H?.formattedWithAbberviations() ?? "")
+        let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+        let marketCapChangeStat =  StatisticsModel(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange)
+        
+        let blockTime = coinDetailModel?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+        let blockStat = StatisticsModel(title: "Block Time", value: blockTimeString)
+        
+        let hashing = coinDetailModel?.hashingAlgorithm ?? "n/a"
+        let hashingStat = StatisticsModel(title: "Hashing Algorithm", value: hashing)
+        
+        let addionalArray : [StatisticsModel] = [
+            hightStat,lowStat,priceChangeStat,marketCapChangeStat,blockStat,hashingStat
+        ]
+        
+        return addionalArray
+        
+    }
+}
---
+++ CryptoTracker/CryptoTracker/Services/CoinDetailDataService.swift
@@ -0,0 +1,46 @@
+//
+//  CoinDetailDataService.swift
+//  CryptoTracker
+//
+//  Created by Rohit Patil on 19/08/24.
+//
+
+import Foundation
+import Combine
+
+
+class CoinDetailDataService {
+    
+    @Published var coinDetails: CoinDetailModel? = nil
+    
+    var coinDetailSubscription: AnyCancellable?
+    let coin:CoinModel
+    
+    init(coin:CoinModel) {
+        self.coin = coin
+        getCoinsDetails()
+}
+
+    func getCoinsDetails() {
+     
+     //URL : https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=false&communit_data=false&developer_data=false&sparkline=false
+        var components = URLComponents(string: "https://api.coingecko.com/api/v3/coins/\(coin.id)")
+    components?.queryItems = [
+        URLQueryItem(name: "localization", value: "false"),
+        URLQueryItem(name: "tickers", value: "false"),
+        URLQueryItem(name: "market_data", value: "false"),
+        URLQueryItem(name: "communit_data", value: "false"),
+        URLQueryItem(name: "developer_data", value: "false"),
+        URLQueryItem(name: "sparkline", value: "false")
+    ]
+    
+    guard let url = components?.url else { return }
+            
+        coinDetailSubscription = NetworkingManager.download(url: url, method: "GET")
+        .decode(type: CoinDetailModel.self, decoder: JSONDecoder())
+        .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoinDetails) in
+            self?.coinDetails = returnedCoinDetails
+            self?.coinDetailSubscription?.cancel()
+        })
+}
+}
